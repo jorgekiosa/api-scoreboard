783ff7adfcca9dbfd9828e52534cfc5d0ff2d3f3
Jorge Kiosa <jorge.kiosa@ucall.co.ao>
2025-01-02 20:39:40 +0100

    add .env

--- server.js
+++ server.js
@@ -21,33 +21,13 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', process.env.FRONT_BASE_URL);
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
 const io = new Server(server, {
   cors: corsOptions, // CORS no Socket.IO
 });
 
 // Teste para verificar o servidor
 app.get('/', (req, res) => {
-  res.send('Servidor WebSocket rodando!');
+  res.send('Servidor WebSocket rodando!', corsOptions);
 });
 
 const connectedClients = {};
